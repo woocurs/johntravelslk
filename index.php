86c5f21cacc23f2527cdd43d55a392b5fc5d6f82
FathimaSafrina <fathimasafrina@woocurs.com>
2024-11-18 18:36:49 +0530

    header link with pages

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <div class="left-content">
                 <h1>Explore the Pearl of Indian Ocean</h1>
                 <p>Experience the adventures of Mirabilis: eat, sleep, hike, climb, fly, jump, swim, snorkel, surf, and dive.</p>
-                <a href="explore.php" class="button">     Explore More   </a>
+                <a href="tourpackages.php" class="button">     Explore More   </a>
             </div>
             <div class="vertical-line"></div>
             <div class="right-content">
@@ -22,7 +22,7 @@
             <div class="left-content">
                 <h1>Let's Make Memories</h1>
                 <p>Make unforgettable memories with romantic and adventurous experiences.</p>
-                <a href="explore.php" class="button">     Explore More   </a>
+                <a href="tourpackages.php" class="button">     Explore More   </a>
             </div>
             <div class="right-content">
                 <p>Find joy in every moment, from scenic sunsets to thrilling outdoor activities.</p>
