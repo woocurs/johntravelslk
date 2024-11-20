5ff3d3ca20bb417b232c8db52c786b7b823d44f7
SatheesanWoocurs <satheesan@woocurs.com>
2024-11-20 20:44:53 +0530

    changes update

--- careers.php
+++ careers.php
@@ -121,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Career</title>
-    <link rel="stylesheet" href="styles/career.css">
+    <link rel="stylesheet" href="styles/careers.css">
     <link rel="stylesheet" href="styles/css/bootstrap.min.css">
 
     <style>
