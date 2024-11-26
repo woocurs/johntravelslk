fcdc9813194e7938e92fe4ae9225bb19ba9d401d
SatheesanWoocurs <satheesan@woocurs.com>
2024-11-26 22:01:17 +0530

    careers.php updated

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="styles/aboutus.css">
+    <link rel="stylesheet" href="styles/about.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
